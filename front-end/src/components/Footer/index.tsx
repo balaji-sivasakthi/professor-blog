e3eeb0bdde37f6a0e0ffcb1a7ed8476a24c4c85c
Iwin Issac <74755442+iwint@users.noreply.github.com>
2024-02-25 21:48:28 +0530

    fix(footer): changed the download section to contact section (#31)
    * fix(footer): changed the download section to dontact dection
    
    * enhancement(footer): changed the download section to contact section

--- front-end/src/components/Footer/index.tsx
+++ front-end/src/components/Footer/index.tsx
@@ -4,136 +4,126 @@ const Footer = () => {
   return (
     <footer className="bg-gray-50 dark:bg-gray-800 antialiased">
       <div className="p-4 py-6 mx-auto max-w-screen-xl md:p-8 lg:p-10">
-        <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
-          <div>
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              Company
-            </h2>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className=" hover:underline">
-                  About
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Careers
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Brand Center
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Blog
-                </a>
-              </li>
-            </ul>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+          <div className="grid grid-cols-2 place-items-center md:place-items-start lg:place-items-start gap-8 md:grid-cols-2 lg:grid-cols-2">
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                Company
+              </h2>
+              <ul className="text-gray-500 dark:text-gray-400">
+                <li className="mb-4">
+                  <a href="#" className=" hover:underline">
+                    About
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Careers
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Brand Center
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Blog
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                Help center
+              </h2>
+              <ul className="text-gray-500 dark:text-gray-400">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Discord Server
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Twitter
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Facebook
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div>
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              Help center
-            </h2>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Discord Server
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Twitter
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Facebook
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Contact Us
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              Legal
-            </h2>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Privacy Policy
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Licensing
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Terms
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              Company
-            </h2>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className=" hover:underline">
-                  About
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Careers
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Brand Center
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Blog
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              Download
-            </h2>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  iOS
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Android
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Windows
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  MacOS
-                </a>
-              </li>
-            </ul>
+          <div className="flex flex-col gap-2 justify-center lg:justify-start items-center mt-6 lg:mt-0 md:mt-0 lg:items-end">
+            <div className="flex flex-col gap-2">
+              <div className="lg:mt-0 flex lg:col-span-2 lg:flex gap-2">
+                <img
+                  className="rounded-full p-1"
+                  height={80}
+                  width={80}
+                  src="./assets/images/chancellor.png"
+                  alt="mockup"
+                />
+                <div>
+                  <p className="text-gray-500 dark:text-gray-400 text-[14px]">
+                    <strong className="text-white">
+                      Dr. S. Jayarama Reddy, M.Sc., Ph.D
+                    </strong>
+                    <br />
+                    Jayapadma Nivas, 12-13-853/22,
+                    <br />
+                    Street No. 13, Nagarjuna Nagar,
+                    <br />
+                    Tarnaka, Hyderabad -500 017, India.
+                  </p>
+                </div>
+              </div>
+              <div>
+                <p className="text-white text-[14px]">
+                  <strong>Phone :</strong>{" "}
+                  <a
+                    className="hover:underline text-gray-500 dark:text-gray-400"
+                    href="tel:+919949027118"
+                  >
+                    9949027118
+                  </a>
+                  ,
+                  <a
+                    className="hover:underline text-gray-500 dark:text-gray-400"
+                    href="tel:+917989290324"
+                  >
+                    7989290324
+                  </a>
+                  <br />
+                  <strong>Email :</strong>{" "}
+                  <a
+                    className="hover:underline text-gray-500 dark:text-gray-400"
+                    href="mailto:profsjr@gmail.com"
+                  >
+                    profsjr@gmail.com
+                  </a>
+                  ,
+                  <a
+                    className="hover:underline text-gray-500 dark:text-gray-400"
+                    href="mailto:chancellor.sjr@gmail.com"
+                  >
+                    chancellor.sjr@gmail.com
+                  </a>
+                  <br />
+                  <p className="text-gray-500 dark:text-gray-400">
+                    <strong className="text-white">Hours : </strong>
+                    8:30am – 5:00pm Monday – Friday
+                  </p>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
