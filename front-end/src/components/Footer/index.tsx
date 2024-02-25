b08be122f8b96cc3f314ab89ce8ec1e9261dba0f
iwint <iwint.cseasnsct@gmail.com>
2024-02-25 20:14:20 +0530

    fix(footer): changed the download section to dontact dection

--- front-end/src/components/Footer/index.tsx
+++ front-end/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-50 dark:bg-gray-800 antialiased">
       <div className="p-4 py-6 mx-auto max-w-screen-xl md:p-8 lg:p-10">
-        <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
+        <div className="grid grid-cols-2 place-items-center gap-8 md:grid-cols-3 lg:grid-cols-3">
           <div>
             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
               Company
@@ -59,81 +59,56 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div>
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              Legal
-            </h2>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Privacy Policy
+          <div className="flex col-span-3 md:col-span-1 lg:col-span-1 flex-col gap-2 justify-start items-start">
+            <div className="lg:mt-0 flex lg:col-span-2 lg:flex gap-2">
+              <img
+                className="rounded-full"
+                height={80}
+                width={80}
+                src="./assets/images/chancellor.png"
+                alt="mockup"
+              />
+              <div>
+                <p className="text-white text-[14px]">
+                  <strong>Dr. S. Jayarama Reddy, M.Sc., Ph.D</strong>
+                  <br />
+                  Jayapadma Nivas, 12-13-853/22,
+                  <br />
+                  Street No. 13, Nagarjuna Nagar,
+                  <br />
+                  Tarnaka, Hyderabad -500 017, India.
+                </p>
+              </div>
+            </div>
+            <div>
+              <p className="text-white text-[14px]">
+                <strong>Phone:</strong>{" "}
+                <a className="hover:underline" href="tel:+919949027118">
+                  9949027118
                 </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Licensing
+                ,
+                <a className="hover:underline" href="tel:+917989290324">
+                  7989290324
                 </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Terms
+                <br />
+                <strong>Email:</strong>{" "}
+                <a className="hover:underline" href="mailto:profsjr@gmail.com">
+                  profsjr@gmail.com
                 </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              Company
-            </h2>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className=" hover:underline">
-                  About
+                ,
+                <a
+                  className="hover:underline"
+                  href="mailto:chancellor.sjr@gmail.com"
+                >
+                  chancellor.sjr@gmail.com
                 </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Careers
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Brand Center
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Blog
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              Download
-            </h2>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  iOS
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Android
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Windows
-                </a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  MacOS
-                </a>
-              </li>
-            </ul>
+                <br />
+                <p className="">
+                  <strong>Hours: </strong>
+                  8:30am – 5:00pm Monday – Friday
+                </p>
+              </p>
+            </div>
           </div>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
