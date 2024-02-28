a2ff76838063a7e26c38d8521eb5450e70e5de9b
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2024-02-29 01:20:30 +0530

    quick fix: schema change

--- front-end/src/app/blog/[id]/page.tsx
+++ front-end/src/app/blog/[id]/page.tsx
@@ -12,55 +12,33 @@ const Content = () => {
   );
   const originalData = data?.data?.attributes;
   console.log(originalData);
-  const urlLink = originalData?.Image?.data[0]?.attributes?.formats?.large?.url;
+
+  let urlLink;
+  if (originalData?.Image?.data && originalData?.Image?.data[0]) {
+    urlLink = originalData?.Image?.data[0]?.attributes?.formats?.large?.url;
+  }
   console.log(`http://localhost:1337${urlLink}`);
   const profileLink = originalData?.Profile?.data?.attributes?.url;
   console.log(profileLink);
 
-  // http://localhost:1337/uploads/large_Programs_8232adea14.png
   return (
     <div>
-      <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-whiteantialiased">
+      <main className="pt-8 pb-16 lg:pt-16 lg:pb-24">
         <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
-          <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue">
+          <article className="mx-auto w-full max-w-3xl format format-sm sm:format-base lg:format-lg format-blue">
             <header className="mb-4 lg:mb-6 not-format">
-              <address className="flex items-center mb-6 not-italic">
-                <div className="inline-flex items-center mr-3 text-sm text-gray-900">
-                  <img
-                    className="mr-4 w-32 h-32 rounded-full"
-                    src={`http://localhost:1337${profileLink}`}
-                    alt="Jese Leos"
-                  />
-                  <div>
-                    <a
-                      href="#"
-                      rel="author"
-                      className="text-xl font-bold text-gray-900"
-                    >
-                      {originalData?.Author}
-                    </a>
-                    <p className="text-base text-gray-500">
-                      {originalData?.Designation}
-                    </p>
-                    <p className="text-base text-gray-500">
-                      <time dateTime="2022-02-08" title="February 8th, 2022">
-                        {originalData?.publishedAt}
-                      </time>
-                    </p>
-                  </div>
-                </div>
-              </address>
               <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">
                 {originalData?.Title}
               </h1>
               <p className="lead">{originalData?.Description}</p>
             </header>
             <div>
-              <img
-                className="mr-4 w-full h-full"
-                src={`http://localhost:1337${urlLink}`}
-                alt=""
-              />
+              {urlLink && (
+                <img
+                  className="mr-4 w-full h-full"
+                  src={`http://localhost:1337${urlLink}`}
+                />
+              )}
             </div>
           </article>
         </div>
--- front-end/src/components/Card/index.tsx
+++ front-end/src/components/Card/index.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, description, svgPath, id }) => {
   return (
-    <div className="p-4 md:w-1/3">
+    <div className="p-4">
       <div className="flex rounded-lg h-full bg-white p-8 flex-col">
         <div className="flex items-center mb-3">
           <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
